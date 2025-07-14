bf882ab8f3188e9e44e349ecfee1ccd0458cbecb
Om Tewari <omtewari106@gmail.com>
2025-07-14 13:22:14 +0530

    removing duplicate js folders

--- src/api/products.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const BASE_URL = 'https://fakestoreapi.com';
-export const fetchAllProducts = () => axios.get(`${BASE_URL}/products`);
-export const fetchCategories = () => axios.get(`${BASE_URL}/products/categories`);
-export const fetchProductsByCategory = (category) => axios.get(`${BASE_URL}/products/category/${category}`);
-export const fetchProductById = (id) => axios.get(`${BASE_URL}/products/${id}`);
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './slice/productSlice';
-import cartReducer from './slice/cartSlice';
-export const store = configureStore({
-    reducer: {
-        products: productReducer,
-        cart: cartReducer
-    },
-});
